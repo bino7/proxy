5bc18df62aa47603aca3aa88c7686ad3df6d3ba8
EJUNBIN <junbin.zhong@ericsson.com>
2017-01-11 14:45:29 +0800

    ~

--- proxy.go
+++ proxy.go
@@ -41,12 +41,11 @@ import (
 	_ "github.com/google/martian/stash"
 	_ "github.com/google/martian/static"
 	_ "github.com/google/martian/status"
-	"fmt"
 )
 
 var (
 	level          = flag.Int("v", 0, "log level")
-	//addr           = flag.String("addr", ":8080", "host:port of the proxy")
+	addr           = flag.String("addr", ":80", "host:port of the proxy")
 	apiAddr        = flag.String("api-addr", ":8181", "port of the configuration api")
 	tlsAddr        = flag.String("tls-addr", ":4443", "host:port of the proxy over TLS")
 	api            = flag.String("api", "martian.proxy", "hostname for the API")
@@ -62,9 +61,6 @@ var (
 )
 
 func main() {
-
-	port := os.Getenv("PORT")
-	addr := fmt.Sprintf(":%s", port)
 
 	flag.Parse()
 
@@ -187,7 +183,7 @@ func main() {
 	rh.SetResponseVerifier(m)
 	configure("/verify/reset", rh)
 
-	l, err := net.Listen("tcp", addr)
+	l, err := net.Listen("tcp", *addr)
 	if err != nil {
 		log.Fatal(err)
 	}
