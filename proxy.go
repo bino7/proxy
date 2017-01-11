f925dff565fff69e7fb3a7f76f80f00eeaf66a08
EJUNBIN <junbin.zhong@ericsson.com>
2017-01-11 14:34:11 +0800

    Initial commit

---
+++ proxy.go
@@ -0,0 +1,230 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"flag"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"path"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/google/martian"
+	mapi "github.com/google/martian/api"
+	"github.com/google/martian/cors"
+	"github.com/google/martian/fifo"
+	"github.com/google/martian/har"
+	"github.com/google/martian/httpspec"
+	mlog "github.com/google/martian/log"
+	"github.com/google/martian/marbl"
+	"github.com/google/martian/martianhttp"
+	"github.com/google/martian/martianlog"
+	"github.com/google/martian/mitm"
+	"github.com/google/martian/servemux"
+	"github.com/google/martian/trafficshape"
+	"github.com/google/martian/verify"
+
+	_ "github.com/google/martian/body"
+	_ "github.com/google/martian/cookie"
+	_ "github.com/google/martian/martianurl"
+	_ "github.com/google/martian/method"
+	_ "github.com/google/martian/pingback"
+	_ "github.com/google/martian/port"
+	_ "github.com/google/martian/priority"
+	_ "github.com/google/martian/querystring"
+	_ "github.com/google/martian/skip"
+	_ "github.com/google/martian/stash"
+	_ "github.com/google/martian/static"
+	_ "github.com/google/martian/status"
+	"fmt"
+)
+
+var (
+	level          = flag.Int("v", 0, "log level")
+	//addr           = flag.String("addr", ":8080", "host:port of the proxy")
+	apiAddr        = flag.String("api-addr", ":8181", "port of the configuration api")
+	tlsAddr        = flag.String("tls-addr", ":4443", "host:port of the proxy over TLS")
+	api            = flag.String("api", "martian.proxy", "hostname for the API")
+	generateCA     = flag.Bool("generate-ca-cert", false, "generate CA certificate and private key for MITM")
+	cert           = flag.String("cert", "", "CA certificate used to sign MITM certificates")
+	key            = flag.String("key", "", "private key of the CA used to sign MITM certificates")
+	organization   = flag.String("organization", "Martian Proxy", "organization name for MITM certificates")
+	validity       = flag.Duration("validity", time.Hour, "window of time that MITM certificates are valid")
+	allowCORS      = flag.Bool("cors", false, "allow CORS requests to configure the proxy")
+	harLogging     = flag.Bool("har", false, "enable HAR logging API")
+	trafficShaping = flag.Bool("traffic-shaping", false, "enable traffic shaping API")
+	skipTLSVerify  = flag.Bool("skip-tls-verify", false, "skip TLS server verification; insecure")
+)
+
+func main() {
+
+	port := os.Getenv("PORT")
+	addr := fmt.Sprintf(":%s", port)
+
+	flag.Parse()
+
+	mlog.SetLevel(*level)
+
+	p := martian.NewProxy()
+
+	var x509c *x509.Certificate
+	var priv interface{}
+
+	if *generateCA {
+		var err error
+		x509c, priv, err = mitm.NewAuthority("martian.proxy", "Martian Authority", 30*24*time.Hour)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else if *cert != "" && *key != "" {
+		tlsc, err := tls.LoadX509KeyPair(*cert, *key)
+		if err != nil {
+			log.Fatal(err)
+		}
+		priv = tlsc.PrivateKey
+
+		x509c, err = x509.ParseCertificate(tlsc.Certificate[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if x509c != nil && priv != nil {
+		mc, err := mitm.NewConfig(x509c, priv)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		mc.SetValidity(*validity)
+		mc.SetOrganization(*organization)
+		mc.SkipTLSVerify(*skipTLSVerify)
+
+		p.SetMITM(mc)
+
+		// Expose certificate authority.
+		ah := martianhttp.NewAuthorityHandler(x509c)
+		configure("/authority.cer", ah)
+
+		// Start TLS listener for transparent MITM.
+		tl, err := net.Listen("tcp", *tlsAddr)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go p.Serve(tls.NewListener(tl, mc.TLS()))
+	}
+
+	stack, fg := httpspec.NewStack("martian")
+
+	// wrap stack in a group so that we can forward API requests to the API port
+	// before the httpspec modifiers which include the via modifier which will
+	// trip loop detection
+	topg := fifo.NewGroup()
+
+	// Redirect API traffic to API server.
+	if *apiAddr != "" {
+		apip := strings.Replace(*apiAddr, ":", "", 1)
+		port, err := strconv.Atoi(apip)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Forward traffic that pattern matches in http.DefaultServeMux
+		apif := servemux.NewFilter(nil)
+		apif.SetRequestModifier(mapi.NewForwarder("", port))
+		topg.AddRequestModifier(apif)
+	}
+	topg.AddRequestModifier(stack)
+	topg.AddResponseModifier(stack)
+
+	p.SetRequestModifier(topg)
+	p.SetResponseModifier(topg)
+
+	m := martianhttp.NewModifier()
+	fg.AddRequestModifier(m)
+	fg.AddResponseModifier(m)
+
+	if *harLogging {
+		hl := har.NewLogger()
+		stack.AddRequestModifier(hl)
+		stack.AddResponseModifier(hl)
+
+		configure("/logs", har.NewExportHandler(hl))
+		configure("/logs/reset", har.NewResetHandler(hl))
+	}
+
+	logger := martianlog.NewLogger()
+	logger.SetDecode(true)
+
+	stack.AddRequestModifier(logger)
+	stack.AddResponseModifier(logger)
+
+	lsh := marbl.NewHandler()
+	// retrieve binary marbl logs
+	http.Handle("/binlogs", lsh)
+
+	lsm := marbl.NewModifier(lsh)
+	stack.AddRequestModifier(lsm)
+	stack.AddResponseModifier(lsm)
+
+	// Configure modifiers.
+	configure("/configure", m)
+
+	// Verify assertions.
+	vh := verify.NewHandler()
+	vh.SetRequestVerifier(m)
+	vh.SetResponseVerifier(m)
+	configure("/verify", vh)
+
+	// Reset verifications.
+	rh := verify.NewResetHandler()
+	rh.SetRequestVerifier(m)
+	rh.SetResponseVerifier(m)
+	configure("/verify/reset", rh)
+
+	l, err := net.Listen("tcp", *addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if *trafficShaping {
+		tsl := trafficshape.NewListener(l)
+		tsh := trafficshape.NewHandler(tsl)
+		configure("/shape-traffic", tsh)
+
+		l = tsl
+	}
+
+	log.Println("martian: proxy started on:", l.Addr())
+
+	go p.Serve(l)
+
+	go http.ListenAndServe(*apiAddr, nil)
+
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, os.Interrupt, os.Kill)
+
+	<-sigc
+
+	log.Println("martian: shutting down")
+}
+
+// configure installs a configuration handler at path.
+func configure(pattern string, handler http.Handler) {
+	if *allowCORS {
+		handler = cors.NewHandler(handler)
+	}
+
+	// register handler for martian.proxy to be forwarded to
+	// local API server
+	http.Handle(path.Join(*api, pattern), handler)
+
+	// register handler for local API server
+	p := path.Join("localhost"+*apiAddr, pattern)
+	http.Handle(p, handler)
+}
---
+++ vendor/github.com/google/martian/static/static_file_modifier.go
@@ -0,0 +1,131 @@
+// Copyright 2015 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package static provides a modifier that allows Martian to return static files
+// local to Martian. The static modifier does not support setting explicit path
+// mappings via the JSON API.
+package static
+
+import (
+	"encoding/json"
+	"mime"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+
+	"github.com/google/martian"
+	"github.com/google/martian/parse"
+)
+
+// Modifier is a martian.RequestResponseModifier that routes reqeusts to rootPath
+// and serves the assets there, while skipping the HTTP roundtrip.
+type Modifier struct {
+	rootPath      string
+	explicitPaths map[string]string
+}
+
+type staticJSON struct {
+	ExplicitPaths map[string]string    `json:"explicitPaths"`
+	RootPath      string               `json:"rootPath"`
+	Scope         []parse.ModifierType `json:"scope"`
+}
+
+func init() {
+	parse.Register("static.Modifier", modifierFromJSON)
+}
+
+// NewModifier constructs a static.Modifier that takes a path to serve files from, as well as an optional mapping of request paths to local
+// file paths (still rooted at rootPath).
+func NewModifier(rootPath string) *Modifier {
+	return &Modifier{
+		rootPath:      path.Clean(rootPath),
+		explicitPaths: make(map[string]string),
+	}
+}
+
+// ModifyRequest marks the context to skip the roundtrip and downgrades any https requests
+// to http.
+func (s *Modifier) ModifyRequest(req *http.Request) error {
+	ctx := martian.NewContext(req)
+	ctx.SkipRoundTrip()
+
+	if req.URL.Scheme == "https" {
+		req.URL.Scheme = "http"
+		ctx.Session().MarkInsecure()
+	}
+
+	return nil
+}
+
+// ModifyResponse reads the file rooted at rootPath joined with the request URL
+// path. In the case that the the request path is a key in s.explicitPaths, ModifyRequest
+// will attempt to open the file located at s.rootPath joined by the value in s.explicitPaths
+// (keyed by res.Request.URL.Path).  In the case that the file cannot be found, the response
+// will be a 404. ModifyResponse will return a 404 for any path that is defined in s.explictPaths
+// and that does not exist locally, even if that file does exist in s.rootPath.
+func (s *Modifier) ModifyResponse(res *http.Response) error {
+	reqpth := filepath.Clean(res.Request.URL.Path)
+	fpth := filepath.Join(s.rootPath, reqpth)
+
+	if _, ok := s.explicitPaths[reqpth]; ok {
+		fpth = filepath.Join(s.rootPath, s.explicitPaths[reqpth])
+	}
+
+	f, err := os.Open(fpth)
+	switch {
+	case os.IsNotExist(err):
+		res.StatusCode = http.StatusNotFound
+		return nil
+	case os.IsPermission(err):
+		// This is returning a StatusUnauthorized to reflect that the Martian does
+		// not have the appropriate permissions on the local file system.  This is a
+		// deviation from the standard assumption around an HTTP 401 response.
+		res.StatusCode = http.StatusUnauthorized
+		return err
+	case err != nil:
+		res.StatusCode = http.StatusInternalServerError
+		return err
+	}
+
+	res.Body.Close()
+	res.Body = f
+
+	info, err := f.Stat()
+	if err != nil {
+		res.StatusCode = http.StatusInternalServerError
+		return err
+	}
+	res.ContentLength = info.Size()
+	res.Header.Set("Content-Type", mime.TypeByExtension(filepath.Ext(fpth)))
+
+	return nil
+}
+
+// SetExplicitPathMappings sets an optional mapping of request paths to local
+// file paths rooted at s.rootPath.
+func (s *Modifier) SetExplicitPathMappings(ep map[string]string) {
+	s.explicitPaths = ep
+}
+
+func modifierFromJSON(b []byte) (*parse.Result, error) {
+	msg := &staticJSON{}
+	if err := json.Unmarshal(b, msg); err != nil {
+		return nil, err
+	}
+
+	mod := NewModifier(msg.RootPath)
+	mod.SetExplicitPathMappings(msg.ExplicitPaths)
+	return parse.NewResult(mod, msg.Scope)
+}
